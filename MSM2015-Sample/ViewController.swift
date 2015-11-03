a4413ea7e2bd617e688bc4f8a00c1b441953b7a7
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-11-03 20:01:40 +0900

    Initial Commit

---
+++ MSM2015-Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSM2015-Sample
+//
+//  Created by Tomohiro Kumagai on H27/11/03.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
